9511e9a5c5c7923991ebfe542edb6f27ac543477
riicardods <riicardoljn@icloud.com>
2024-01-05 20:03:05 -0300

    A

--- ProEventos.API/Data/DataContext.cs
+++ ProEventos.API/Data/DataContext.cs
@@ -6,5 +6,11 @@ namespace ProEventos.API.Data
     public class DataContext : DbContext
     {
         public DbSet <Evento>  Eventos { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+
+        }
     }
 }
---
+++ ProEventos.API/Migrations/20240105225644_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProEventos.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Eventos",
+                columns: table => new
+                {
+                    EventoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Local = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DataEvento = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Tema = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    QtdPessoas = table.Column<int>(type: "int", nullable: false),
+                    Lote = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImagemURL = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Eventos", x => x.EventoId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Eventos");
+        }
+    }
+}
