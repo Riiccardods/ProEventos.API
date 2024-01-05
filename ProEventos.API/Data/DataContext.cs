1965a6958c5d37f6222315aee5dfb7af35560c40
riicardods <riicardoljn@icloud.com>
2024-01-05 19:44:51 -0300

    A

--- ProEventos.API/Data/DataContext.cs
+++ ProEventos.API/Data/DataContext.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using ProEventos.API.Models;
 
 namespace ProEventos.API.Data
 {
     public class DataContext : DbContext
     {
+        public DbSet <Evento>  Eventos { get; set; }
     }
 }
--- ProEventos.API/Program.cs
+++ ProEventos.API/Program.cs
@@ -1,21 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using ProEventos.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// configuring Swagger/OpenAPI
+
+////aqui codigo que nao tenho que decorar
+builder.Services.AddDbContext<DataContext>(options => /*configura o contexto do Entity Framework Core para interagir com o banco de dados.*/
+options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoSQL"))); /*configura o contexto do Entity Framework Core para interagir com o banco de dados.*/
+////aqui codigo que nao tenho que decorar
+
+
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+app.UseSwagger();
+app.UseSwaggerUI();
+//}
 
 app.UseHttpsRedirection();
 
