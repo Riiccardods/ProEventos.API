7463df4d3822ba453222fdbe071a499d13cd0667
riicardods <riicardoljn@icloud.com>
2024-02-26 21:19:44 -0300

    A

--- ProEventos.API/Models/Evento.cs
+++ ProEventos.API/Models/Evento.cs
@@ -8,6 +8,7 @@
         public string Local { get; set; }
 
         public string DataEvento { get; set; }
+
         public string Tema { get; set; }
         public int QtdPessoas { get; set; }
         public string Lote { get; set; }
